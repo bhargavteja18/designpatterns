39fe91fe3903ce65c7ffb90969b08a8501a05718
bhargavteja18 <bhargavteja38@gmail.com>
2022-10-30 17:01:42 +0530

    builder pattern

---
+++ src/builder/Main.java
@@ -0,0 +1,14 @@
+package builder;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User.UserBuilder userBuilder=User.builder();
+        User user = userBuilder.id(1).name("bhargav").city("pdtr").build();
+        System.out.println(user);
+
+    }
+}
